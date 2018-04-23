700caadc8862391e64c19ef6966390f88d56a54b
lipeng.hu@itdev.xiangguang.com <lipeng.hu@itdev.xiangguang.com>
2018-04-23 16:23:41 +0800

    [leopen] add use function demo

---
+++ base-demo/python-function/use-function.py
@@ -0,0 +1,15 @@
+print('abs -20:', abs(-20))
+print('max', max(1, 2, 3, 4))
+
+# type convert
+str1 = '111'
+str2 = '111.23'
+str3 = ''
+num1 = 12.34
+num2 = 0
+num3 = 12
+
+print('convert to int', int(str1), int(12.34))
+print('convert to float', float(num3), float(str2))
+print('convert to str', str(num1))
+print('convert to Bool', bool(num1), bool(str3), bool(num2))
